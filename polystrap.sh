9b490a5e33e1ab683e18c956de4b6af5ffa0c905
josch <josch@pyneo.org>
2011-10-18 15:25:46 +0200

    add hint about existing root directory

--- polystrap.sh
+++ polystrap.sh
@@ -93,7 +93,7 @@ echo "I: pkgs:    $PACKAGES"
 echo "I: --------------------------"
 
 [ -e "$ROOTDIR.tar" ] && { echo "tarball still exists" >&2; exit 1; }
-[ -e "$ROOTDIR" ] && { echo "root directory still exists" >&2; exit 1; }
+[ -e "$ROOTDIR" ] && { echo "root directory $ROOTDIR still exists" >&2; exit 1; }
 
 # create multistrap.conf
 echo "I: create multistrap.conf"
