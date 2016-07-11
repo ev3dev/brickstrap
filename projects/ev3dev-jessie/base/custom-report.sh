9d07aa9b8cff116ab6b25ede561957018d6d0ec8
David Lechner <david@lechnology.com>
2016-07-11 18:46:54 -0500

    Start using travis
    Use travis to build all of the supported configurations.
    
    If we push to the deploy branch, it will also deploy the image that was
    created to bintray.

--- projects/ev3dev-jessie/base/custom-report.sh
+++ projects/ev3dev-jessie/base/custom-report.sh
@@ -27,3 +27,38 @@ Included Packages
 $(br_chroot dpkg -l)
 \`\`\`
 EOF
+
+if [ "$TRAVIS" == "true" ]; then
+
+cat > $(br_report_dir)/bintray.json << EOF
+{
+    "package": {
+        "name": "$BR_PROJECT",
+        "repo": "nightly",
+        "subject": "ev3dev",
+        "desc": "SD card images for testing",
+        "website_url": "www.ev3dev.org",
+        "issue_tracker_url": "https://github.com/ev3dev/ev3dev/issues",
+        "vcs_url": "https://github.com/ev3dev/ev3dev.git",
+        "github_use_tag_release_notes": false,
+        "licenses": ["GPL-2.0"],
+        "public_download_numbers": true,
+        "public_stats": true
+    },
+
+    "version": {
+        "name": "$(date --iso-8601)",
+        "desc": "Unsupported image for testing.",
+        "released": "$(date --iso-8601=seconds)",
+        "gpgSign": false
+    },
+
+    "files":
+        [
+        {"includePattern": "$(br_image_dir)/(.*).xz", "uploadPattern": "\$1"}
+        ],
+    "publish": true
+}
+EOF
+
+fi
