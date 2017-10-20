9076b2e34392d9d2432a2b68c9730276a996c7d5
David Lechner <david@lechnology.com>
2017-10-20 16:28:13 -0500

    release: update for PPA
    now using launchpad instead of archive.ev3dev.org

--- release.sh
+++ release.sh
@@ -7,13 +7,8 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 
-OS=ubuntu DIST=trusty ARCH=amd64 pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/ubuntu/trusty-amd64/${source}_${version}_amd64.changes
-dput ev3dev-ubuntu ~/pbuilder-ev3dev/ubuntu/trusty-amd64/${source}_${version}_amd64.changes
+debuild -S
+
+dput ppa:ev3dev/tools ../${source}_${version}_source.changes
 
 gbp buildpackage --git-tag-only
-
-ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/ubuntu includedsc xenial \
-    ~/reprepro/ubuntu/pool/main/${source:0:1}/${source}/${source}_${version}.dsc"
-ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/ubuntu includedeb xenial \
-    ~/reprepro/ubuntu/pool/main/${source:0:1}/${source}/brickstrap_${version}_all.deb"
